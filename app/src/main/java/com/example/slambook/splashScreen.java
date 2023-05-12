43385962418bf1eaa27b2ba27e8af31b09957f0a
Adrian <adrian.rodriguez.x@bulsu.edu.ph>
2023-05-12 21:10:57 +0800

    implemented splash screen

---
+++ app/src/main/java/com/example/slambook/splashScreen.java
@@ -0,0 +1,26 @@
+package com.example.slambook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(splashScreen.this, signIn.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
+    }
+}
